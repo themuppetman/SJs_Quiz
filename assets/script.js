92680a5b2a9b401c358b71638ffaa4993d357ed8
themuppetman <sethjackson@sethshows.com>
2024-03-04 16:38:58 -0800

    JS questions need re-writting

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,45 @@
-const selectOption = [
+const questions = [
     {
         question: "What are arrays enclosed in?",
-        options: ["Square Brackets", "Curly Brackets", "Parentheses", "Single Quotes"],
+        answers: ["Square Brackets", "Curly Brackets", "Parentheses", "Single Quotes"],
         correctAnswer: "Square Brackets"
     },
     {
         question: "Who originally wrote Javascript?",
-        options: ["Tim Berners-lee", "Seth Jackson", "Brendan Eich", "Jordan Walke"],
+        answers: ["Tim Berners-lee", "Seth Jackson", "Brendan Eich", "Jordan Walke"],
         correctAnswer: "Brendan Eich"
     },
     // Add more questions before submission
 ];
 
-const quizContainer = document.getElementById('quiz-container');
-const questionElement = document.getElementById('question');
-const optionsContainer = document.getElementById('options-container');
-const submitButton = document.getElementById('submit-button');
-const resultElement = document.getElementById('result');
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestion = 0;
 let score = 0;
  
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion ();
+}
+
 function showQuestion() {
-    const questionText = document.getElementById("question");
-    questionText.textContent = questions[currentQuestion].question;
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    const options = document.querySelectorAll(".options");
-    options.forEach((options, index) => {
-        choice.textContent = questions[currentQuestion].options[index];
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
     });
-
-    const feedback = document.getElementById("feedback");
-    feedback.textContent = "";
 }
 
-const feedback = document.getElementById("feedback");
-if (selected === questions[currentQuestion].correct) {
-    feedback.textContent = "Correct!";
-    correctAnswer++;
-} 
-else {
-  feedback.textContent = "Incorrect.";  
-  
-  setTimeout(() => {
-    currentQuestion++;
+startQuiz();
 
-    if (currentQuestion < questions.length) {
-        showQuestion();
-    } else {
-      const quizContainer = document.querySelector(".quiz-container");
-      quizContainer.innerHTML = '<p>You got ${correctAnswers} out of ${questions.length} questions.</p>';  
-    }
-  }, 2000);  
-}
 
 
